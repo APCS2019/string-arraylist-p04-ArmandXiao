dd595e40783eb3413a9593966eb71b581b54a1f9
ArmandXiao <45375764+ArmandXiao@users.noreply.github.com>
2018-12-13 14:17:59 +0800

    Correction.

--- 20181209-ArmandXiao.java
+++ 20181209-ArmandXiao.java
@@ -1,23 +1,21 @@
 //part (a)
 public String decodeString(ArrayList<StringPart> parts){
     String temp = "";
-    int start = 0;
-    int length = 0;
     for( int i = 0, i< parts.size(), i++){
-      start = parts.get(i).getStart();
-      if( start != -1){
-        length = parts.get(i).getLength(); 
-        temp = masterString.substring( start, start + length);
+      int start = parts.get(i).getStart();
+      int length = parts.get(i).getLength(); 
+      temp += masterString.substring( start, start + length);
       }
     }
     return temp;
  // part (b)
- public ArrayList<StringPart> encodeString(String word){
+ 
+public ArrayList<StringPart> encodeString(String word){
     ArrayList<StringPart> parts = new ArrayList<StringPart>();
-    String temp = "":
-    for(int i =0; i< word.length; i++){
-      temp = word.substring(i,i+1);
-      parts.add( masterString.findPart( temp));
-    }
+    String temp = word:
+    while( temp.length() != 0){
+        StringParts n = findPart(word);
+        parts.add(n);
+        temp = temp.substring(n.getLength()); 
     return parts;
  }
